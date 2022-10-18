bb36bc446811ad0b6cc8d0d946a76143b1700d07
DANIEL2DAM <dmarrom577@g.educaand.es>
2022-10-18 11:38:14 +0200

    Initial commit

---
+++ app/src/main/java/www/iesmurgi/pruebalayout/TableLayout.kt
@@ -0,0 +1,11 @@
+package www.iesmurgi.pruebalayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout)
+    }
+}
