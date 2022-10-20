bd5bcf400c651f6d1374a5fd2d96b9a5dd0f0540
DANIEL2DAM <dmarrom577@g.educaand.es>
2022-10-20 09:11:04 +0200

    insti

---
+++ app/src/main/java/www/iesmurgi/pruebalayout/linearLayout.kt
@@ -0,0 +1,11 @@
+package www.iesmurgi.pruebalayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class linearLayout: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
