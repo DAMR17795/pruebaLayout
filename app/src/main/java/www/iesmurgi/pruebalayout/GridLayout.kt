9d1fdfbf8222b8d295bc9b09e360e2f1fde298fe
DANIEL2DAM <dmarrom577@g.educaand.es>
2022-10-20 13:24:37 +0200

    insti

--- app/src/main/java/www/iesmurgi/pruebalayout/GridLayout.kt
+++ app/src/main/java/www/iesmurgi/pruebalayout/GridLayout.kt
@@ -1,4 +1,11 @@
 package www.iesmurgi.pruebalayout
 
-class GridLayout {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GridLayout: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.grid_layout)
+    }
 }
